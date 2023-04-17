7f7111b8d580c2a76c94f0af969fe62b04192ee0
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2023-04-17 15:30:54 -0700

    Updates

---
+++ adventure.ts
@@ -0,0 +1,5 @@
+namespace sprites.adventure {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="adventure"
+    export const warriorwomanWarriorWoman = image.ofBuffer(hex``);
+}
